72a9eed0255e6f6357d9242fe1d39f4c11768752
Georgy Ashurov <g.ashurov@vkteam.ru>
2026-03-09 02:33:55 +0300

    upd settings

--- woodgood/settings.py
+++ woodgood/settings.py
@@ -89,12 +89,12 @@ SIMPLE_JWT = {
 }
 
 # Настройки CORS
-CORS_ALLOW_ALL_ORIGINS = True  # Для разработки разрешаем все источники
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOW_ALL_ORIGINS = True  # Для разработки разрешаем все источники
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Должен быть в самом верху
